eac06f1ba1b550cee0e408fe40e3185e334958d8
BarryChew <businessatbarry@gmail.com>
2015-12-28 21:20:23 +0800

    Update visits.php

--- visits.php
+++ visits.php
@@ -1,11 +1,12 @@
 <?php
 function postedVisits() {
-}
-
 $website_visits_file = fopen("website_visits.txt", "r") or die("Unable to open file!");
 $visits = fgets($website_visits_file);
 $visits = $visits + 1
 echo fwrite($website_visits_file,$visits);
 fclose($website_visits_file);
+}
+
+
 
 ?>
